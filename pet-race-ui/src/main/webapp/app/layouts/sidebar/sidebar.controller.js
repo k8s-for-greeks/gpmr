9558ed6e962c5c42bd9a40bd32840cb3aa1014dc
Chris Love <clove@cnmconsulting.net>
2016-06-28 02:28:57 -0600

    Intial app deploy (#58)
    * tweaks for working with cluster
    
    * removing local_dc, no worky
    
    * fixing js min stuff

--- pet-race-ui/src/main/webapp/app/layouts/sidebar/sidebar.controller.js
+++ pet-race-ui/src/main/webapp/app/layouts/sidebar/sidebar.controller.js
@@ -34,16 +34,7 @@
             $state.go('home');
         }
 
-        $MENU_TOGGLE = $('#menu_toggle');
-        $SIDEBAR_MENU = $('#sidebar-menu');
-        $SIDEBAR_FOOTER = $('.sidebar-footer');
-        $LEFT_COL = $('.left_col');
-        $RIGHT_COL = $('.right_col');
-        $NAV_MENU = $('.nav_menu');
-        $FOOTER = $('footer');
-
-        $SIDEBAR_MENU.find('a[class=col-menu]').on('click', function (ev) {
-            $BODY = $('body');
+        $('#sidebar-menu').find('a[class=col-menu]').on('click', function (ev) {
             var $li = $(this).parent();
 
             if ($li.is('.active')) {
@@ -54,8 +45,8 @@
             } else {
                 // prevent closing menu if we are on child menu
                 if (!$li.parent().is('.child_menu')) {
-                    $SIDEBAR_MENU.find('li').removeClass('active active-sm');
-                    $SIDEBAR_MENU.find('li ul').slideUp();
+                    $('#sidebar-menu').find('li').removeClass('active active-sm');
+                    $('#sidebar-menu').find('li ul').slideUp();
                 }
 
                 $li.addClass('active');
@@ -69,25 +60,24 @@
 
         });
 
-        $MENU_TOGGLE.on('click', function () {
-            $BODY = $('body');
-            if ($BODY.hasClass('nav-md')) {
-                $SIDEBAR_MENU.find('li.active ul').hide();
-                $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+        $('#menu_toggle').on('click', function () {
+            if ($('body').hasClass('nav-md')) {
+                $('#sidebar-menu').find('li.active ul').hide();
+                $('#sidebar-menu').find('li.active').addClass('active-sm').removeClass('active');
             } else {
-                $SIDEBAR_MENU.find('li.active-sm ul').show();
-                $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+                $('#sidebar-menu').find('li.active-sm ul').show();
+                $('#sidebar-menu').find('li.active-sm').addClass('active').removeClass('active-sm');
             }
 
-            $BODY.toggleClass('nav-md nav-sm');
+            $('body').toggleClass('nav-md nav-sm');
 
             setContentHeight();
         });
 
         // check active menu
-        $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+        $('#sidebar-menu').find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
 
-        $SIDEBAR_MENU.find('a').filter(function () {
+        $('#sidebar-menu').find('a').filter(function () {
             return this.href == CURRENT_URL;
         }).parent('li').addClass('current-page').parents('ul').slideDown(function () {
             setContentHeight();
@@ -95,24 +85,23 @@
 
         var setContentHeight = function () {
             // reset height
-            $RIGHT_COL.css('min-height', $(window).height());
-            $BODY = $('body');
+            $('.right_col').css('min-height', $(window).height());
 
-            var bodyHeight = $BODY.outerHeight(),
-                footerHeight = $BODY.hasClass('footer_fixed') ? 0 : $FOOTER.height(),
-                leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
+            var bodyHeight = $('body').outerHeight(),
+                footerHeight = $('body').hasClass('footer_fixed') ? 0 : $('footer').height(),
+                leftColHeight = $('.left_col').eq(1).height() + $('.sidebar-footer').height(),
                 contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
 
             // normalize content
-            contentHeight -= $NAV_MENU.height() + footerHeight;
+            contentHeight -= $('.nav_menu').height() + footerHeight;
 
-            $RIGHT_COL.css('min-height', contentHeight);
+            $('.right_col').css('min-height', contentHeight);
         };
 
         // check active menu
-        $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+        $('#sidebar-menu').find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
 
-        $SIDEBAR_MENU.find('a').filter(function () {
+        $('#sidebar-menu').find('a').filter(function () {
             return this.href == CURRENT_URL;
         }).parent('li').addClass('current-page').parents('ul').slideDown(function () {
             setContentHeight();
